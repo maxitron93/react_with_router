87084f7317b009eb139db0b837b459b7c36dbace
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-06-23 17:40:25 +1000

    Add mini-css-extract-plugin and enable source maps for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,57 @@
 const path = require ('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-module.exports = {
-  entry: ['babel-polyfill', './src/js/index.js'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  devServer: {
-    contentBase: './dist',
-    historyApiFallback: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html'
-    })
-  ],
-  devtool: 'cheap-module-eval-source-map'
+module.exports = (env) => {
+  const isProduction = env === 'production'
+  console.log("Production environment:", isProduction)
+  return {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }              
+            }
+          ]
+        }
+      ]
+    },
+    devServer: {
+      contentBase: './dist',
+      historyApiFallback: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'assets/css/styles.css',
+        chunkFilename: 'assets/css/styles.css'
+      })
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map'
+  }
 }
